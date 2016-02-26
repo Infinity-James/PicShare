b36270468bc18a99c1d0ccb76e9abed899778597
James Valaitis <james@infinityjames.com>
2016-02-26 19:17:54 +0000

    Created a file to define representing data in the UI as well as table view controller and a storyboard.

---
+++ PicShare/Model/DataFormatting.swift
@@ -0,0 +1,9 @@
+//
+//  DataFormatting.swift
+//  PicShare
+//
+//  Created by James Valaitis on 26/02/2016.
+//  Copyright © 2016 &Beyond. All rights reserved.
+//
+
+import Foundation
---
+++ PicShare/Model/PicShareResources.swift
@@ -0,0 +1,9 @@
+//
+//  PicShareResources.swift
+//  PicShare
+//
+//  Created by James Valaitis on 26/02/2016.
+//  Copyright © 2016 &Beyond. All rights reserved.
+//
+
+import Foundation
