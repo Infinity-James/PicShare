e5f5cc6214f636868fcad57dc427ad886f457d4b
James Valaitis <james@infinityjames.com>
2016-02-26 19:19:03 +0000

    Implemented StringRepresentable and made User conform to it.

--- PicShare/Model/DataFormatting.swift
+++ PicShare/Model/DataFormatting.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+//	MARK: String Representable
+
+/**
+    `StringRepresentable`
+
+    A protocol that defines conforming object a representable as a String.
+ */
+protocol StringRepresentable {
+    /// A short version of the string representation of the object.
+    var shortString: String { get }
+    /// A medium version of the string representation of the object.
+    var mediumString: String { get }
+    /// A long version of the string representation of the object.
+    var longString: String { get }
+}
+
+extension NSObjectProtocol where Self: StringRepresentable {
+    /// Automatically implement description for objects that can be represented as strings.
+    var description: String { return longString }
+}
+
+extension StringRepresentable where Self: User {
+    var shortString: String { return name }
+    var mediumString: String { return name + " (" + email + ") - " + company.catchPhrase }
+    var longString: String { return name + " (" + email + ") - " + company.catchPhrase }
+}
