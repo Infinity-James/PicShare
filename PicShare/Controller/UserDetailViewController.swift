875719b135eb2b5facdce73f752af23fcff375d1
James Valaitis <james@infinityjames.com>
2016-02-29 22:55:11 +0000

    Setting up the user detail view controller.

---
+++ PicShare/Controller/UserDetailViewController.swift
@@ -0,0 +1,50 @@
+//
+//  UserDetailViewController.swift
+//  PicShare
+//
+//  Created by James Valaitis on 29/02/2016.
+//  Copyright © 2016 &Beyond. All rights reserved.
+//
+
+import UIKit
+
+//	MARK: User Detail View Controller
+
+/**
+    `UserDetailViewController`
+
+    Displays details about the user as well as their albums.
+ */
+class UserDetailViewController: UIViewController {
+    
+    //	MARK: Properties - State
+    
+    var user: User {
+        didSet {
+            nameLabel.text = user.name
+            usernameLabel.text = user.username
+            companyLabel.text = user.company.name
+            companyBSLabel.text = user.company.bull
+            emailLabel.text = user
+        }
+    }
+    
+    //	MARK: Properties - Subviews
+    
+    /// The table view which lists the user's albums.
+    @IBOutlet var tableView: UITableView!
+    /// Displays the user's full name.
+    @IBOutlet var nameLabel: UILabel!
+    /// Displays the user's username.
+    @IBOutlet var usernameLabel: UILabel!
+    /// Displays the user's Company name.
+    @IBOutlet var companyLabel: UILabel!
+    /// Displays the bull that company pushes as their 'USP'.
+    @IBOutlet var companyBSLabel: UILabel!
+    /// Displays the user's email address.
+    @IBOutlet var emailLabel: UILabel!
+    /// Displays the user's website.
+    @IBOutlet var websiteLabel: UILabel!
+    
+    
+}
