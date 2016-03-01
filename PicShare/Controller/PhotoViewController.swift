7016ce5f0a31567e9b9d691b2214d4babf566efc
James Valaitis <james@infinityjames.com>
2016-03-01 15:07:27 +0000

    Created a view controller in which to display a photo.

---
+++ PicShare/Controller/PhotoViewController.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoViewController.swift
+//  PicShare
+//
+//  Created by James Valaitis on 01/03/2016.
+//  Copyright © 2016 &Beyond. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+}
